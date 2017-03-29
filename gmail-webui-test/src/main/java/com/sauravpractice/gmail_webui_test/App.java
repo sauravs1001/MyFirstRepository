4c1b09709b75708ece543675ad6a919ef96748d5
Saurav <saurav.kec@gmail.com>
2017-03-29 17:58:06 +0530

    my first commit or code checkin to git

---
+++ gmail-webui-test/src/main/java/com/sauravpractice/gmail_webui_test/App.java
@@ -0,0 +1,13 @@
+package com.sauravpractice.gmail_webui_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ gmail-webui-test/src/test/java/com/sauravpractice/gmail_webui_test/GmailSigninTest.java
@@ -0,0 +1,47 @@
+package com.sauravpractice.gmail_webui_test;
+
+import java.sql.Driver;
+
+import org.junit.After;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GmailSigninTest {
+
+	@Test
+
+	public void gmailSuccessfulLogin() {
+
+		// 1. Go To Gmail Website
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\zik50826\\Downloads\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+		driver.get("http://gmail.com");
+
+		// 2. Fill in Username & Password
+
+		WebElement username = driver.findElement(By.id("Email"));
+		username.clear();
+		username.sendKeys("udemyken@gmail.com");
+		driver.findElement(By.id("next")).click();
+
+		WebElement password = driver.findElement(By.id("Passwd"));
+		password.clear();
+		password.sendKeys("udemyken123");
+
+	}
+
+	@After
+
+	public void tearDown() {
+
+		
+
+	}
+
+}
